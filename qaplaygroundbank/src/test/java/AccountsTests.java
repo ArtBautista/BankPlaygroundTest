09b9e0e1e027084b99bbb404358be4ad0e4e25c6
Art <nomancandothis123@gmail.com>
2026-09-23 20:24:20 +0800

    test(accounts): add data-driven test verifying account list vs detail view values
    Adds testAccountCredentials using @DataProvider to check that name,
    type, and balance shown in the accounts list match the corresponding
    values on the account detail page after clicking view. Retains
    testAccountsHeader for basic header visibility.

--- qaplaygroundbank/src/test/java/AccountsTests.java
+++ qaplaygroundbank/src/test/java/AccountsTests.java
@@ -1,16 +1,19 @@
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.BaseTests;
+import pages.AccountsPage;
+import pages.AccountsPage.AccountButtons;
 
 public class AccountsTests extends BaseTests {
 
     @BeforeMethod
     public void loginAsStandardUser() {
         loginPage.logIntoApplication("standard_user", "bank_sauce");
-        accountsPage.clickSidebarAccountsButton();
+        accountsPage.clickAccountButtons(AccountButtons.SIDEBAR_ACCOUNTS_BUTTON);
     }
 
     @Test 
@@ -18,5 +21,26 @@ public class AccountsTests extends BaseTests {
         Assert.assertTrue(accountsPage.isAccountsHeaderDisplayed(),
                 "Accounts message is not displayed");  
     }
+
+    @DataProvider(name = "accountCredentials")
+    public Object[][] accountCredentials() {
+        return new Object[][] {
+            { AccountsPage.AccountInformation.ACCOUNT_NAME, AccountsPage.AccountDetails.ACCOUNT_DETAILS_NAME },
+            { AccountsPage.AccountInformation.ACCOUNT_TYPE, AccountsPage.AccountDetails.ACCOUNT_DETAILS_TYPE },
+            { AccountsPage.AccountInformation.ACCOUNT_BALANCE, AccountsPage.AccountDetails.ACCOUNT_DETAILS_BALANCE }
+        };
+    }
+   
+    @Test(dataProvider = "accountCredentials")
+    public void testAccountCredentials(AccountsPage.AccountInformation information, AccountsPage.AccountDetails details) {
+        String listValue = accountsPage.checkAccountInformation(information);
+        System.out.println("INFORMATION: " + listValue);
+        accountsPage.clickAccountButtons(AccountsPage.AccountButtons.VIEW_ACTIONS_BUTTON);
+        String detailValue = accountsPage.checkAccountDetails(details);
+        System.out.println("DETAILS: " + detailValue);
+        Assert.assertEquals(detailValue, listValue);
+    }
+
+
     
 }
